dbb43760992f08f84dfcd72360d821b113cf012a
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2024-11-08 10:52:10 +0100

    smt docker

--- smt/smt2/run.sh
+++ smt/smt2/run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-time z3 sort_smt.smt
-time cvc5 --lang smt2 sort_smt.smt
-time python sort3.py -d 1 -m 12
+mkdir -p ../results
+
+echo "CEGIS encoding of SMT:"
+time python -u sort_incr2.py | tee ../results/sort_incr2.txt
+echo "CEGIS minmax:"
+time python -u sort_incr_minmax.py | tee ../results/sort_incr_minmax.txt
+echo "Component based CEGIS:"
+time python -u sort3.py -d 1 -m 12 | tee ../results/sort_component.txt
+echo "Direct SMT encoding:"
+time z3 sort_smt.smt | tee ../results/sort_smt_z3.txt
+echo "Direct SMT encoding using cvc5:"
+time cvc5 --lang smt2 sort_smt.smt | tee ../results/sort_smt_cvc5.txt
--- smt/sygus/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-time cvc5 --lang=sygus2 sort_tuple_command.sy
